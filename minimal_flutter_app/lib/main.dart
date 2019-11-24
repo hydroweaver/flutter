690dec245f60c8c5143ea2c9b57cd28d516d1176
Verma <karan.verma@unilever.com>
2019-11-24 16:21:01 +0530

    interaction

--- minimal_flutter_app/lib/main.dart
+++ minimal_flutter_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix0;
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main(){
   runApp(MaterialApp(
@@ -8,7 +10,40 @@ void main(){
   );
 }
 
+class TextWidget extends StatelessWidget{
+  
+  final String text;
+  final bool strike;
 
+  TextWidget({this.text, this.strike});
+
+  Widget _TextStyle(){
+    if(!strike){
+      return Text(
+        text,
+        style: TextStyle(
+          color: Colors.green[500],
+          decoration: TextDecoration.lineThrough,
+          fontSize: 25,
+        ),
+      );
+    }
+    else{
+      return Text(
+        text,
+        style: TextStyle(
+          color: Colors.green[500],
+          fontSize: 25,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return new Container(child: _TextStyle());
+  }
+}
 
 class ListApp extends StatefulWidget{
   @override
@@ -16,18 +51,49 @@ class ListApp extends StatefulWidget{
 }
 
 class _ListAppState extends State<ListApp>{
+
+  int ctr = 0;
+  bool strike = false;
+
+  void _Counter(){
+    setState(() {
+      ctr += 1;
+    });
+  }
+
+  bool _StrikeThroughAll(){
+    if(!strike){
+      strike = false;
+      return strike;
+    }
+    else{
+      strike = true;
+      return strike;
+    }
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text("data"),
+        title: Text("Statelful Calling Stateless Widgets"),
       ),
       body: Column(
-        verticalDirection: VerticalDirection.up,
         children: <Widget>[
-          Text("1"),
-          Text("2"),
-          Text("3")
+          TextWidget(text: 'Value of this Widget is $ctr', strike: strike,),
+          TextWidget(text: 'Value of this Widget is ${ctr+1}', strike: strike,),
+          RaisedButton(
+            onPressed: _Counter,
+            child: Text("Increment"),
+            textColor: Colors.green[500],
+            elevation: 10,
+          ),
+          RaisedButton(
+            onPressed: _StrikeThroughAll,
+            child: Text("Strike Toggle"),
+            textColor: Colors.green[500],
+            elevation: 10,
+          ),
         ],
       ),
     );
