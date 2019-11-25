94ee9ab0bf0f075e9d2df0dd665aeac9c1f80cf5
Verma <karan.verma@unilever.com>
2019-11-25 21:55:50 +0530

    listview with scroll inside expanded child of a container

--- minimal_flutter_app/lib/main.dart
+++ minimal_flutter_app/lib/main.dart
@@ -1,27 +1,41 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 void main(){
   runApp(MaterialApp(
     title: "Add User Text to List",
-    home: UserText(),
+    home: _MyCustomForm(),
   ));
 }
 
-class UserText extends StatelessWidget{
+class _MyCustomForm extends StatefulWidget{
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
 
-  List todoList = [];
+//LISTVIEW WITHIN EXPANDED CHILD OF COLMN SINCE IT FILLS ALL THE SPACE
+class _MyCustomFormState extends State<_MyCustomForm>{
+  final myController = TextEditingController();
+
+  final List todoList = [];
 
   void addTodo(){
-    todoList.add("Length of the list is ${todoList.length}");
+    todoList.add(myController.text);
+    Fluttertoast.showToast(
+      msg: "${todoList[todoList.length-1]}"
+    );
+    myController.clear();
   }
 
+  @override
+  void dispose(){
+    myController.dispose();
+    super.dispose();
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  Widget build(BuildContext context){
+        return Scaffold(
       appBar: AppBar(
         title: Text("Write Something"),
       ),
@@ -30,11 +44,9 @@ class UserText extends StatelessWidget{
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           TextField(
-            onTap: (){
-              Fluttertoast.showToast(
-                msg: "you're typing something!"
-              );
-            },
+            controller: myController,
+            enabled: true,
+            autofocus: true,
           ),
           RaisedButton(
             onPressed: addTodo,
@@ -42,11 +54,83 @@ class UserText extends StatelessWidget{
             textColor: Colors.blue[300],
             elevation: 10,
           ),
-          ListView(
-            children: <Widget>[],
+          Expanded(
+            child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: todoList.length,
+            itemBuilder: (context, index){
+              final todo = todoList[index];
+              return ListTile(
+                title: Text(todo),
+              );
+            }
+          ),
           )
         ],
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+//LISTVIEW IN COL WITH SHRINKWRAP = TRUE
+/*class _MyCustomFormState extends State<_MyCustomForm>{
+  final myController = TextEditingController();
+
+  final List todoList = [];
+
+  void addTodo(){
+    todoList.add(myController.text);
+    Fluttertoast.showToast(
+      msg: "${todoList[todoList.length-1]}"
+    );
+    myController.clear();
+  }
+
+  @override
+  void dispose(){
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context){
+        return Scaffold(
+      appBar: AppBar(
+        title: Text("Write Something"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextField(
+            controller: myController,
+          ),
+          RaisedButton(
+            onPressed: addTodo,
+            child: Text("Add Todo"),
+            textColor: Colors.blue[300],
+            elevation: 10,
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: todoList.length,
+            itemBuilder: (context, index){
+              final todo = todoList[index];
+              return ListTile(
+                title: Text(todo),
+              );
+            }
+          )
+        ],
+      ),
+    );
+  }
+}*/
