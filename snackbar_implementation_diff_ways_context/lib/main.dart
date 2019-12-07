9357b0a2136e3180dc25961337eb511a0edadbc2
Verma <karan.verma@unilever.com>
2019-12-07 21:03:08 +0530

    wip on snackbar with navigation to show 2 diff impl

---
+++ snackbar_implementation_diff_ways_context/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Snack Bar with Navigation to show two different Implementation based on the documentation",
+    home: Navigator(
+      initialRoute: '',
+    ),
+  ));
+}
