e5772ede41e25869656fd31c1a1d8be8bca972c9
Verma <karan.verma@unilever.com>
2019-12-17 15:46:41 +0530

    wip enable camera

--- dart files for testing/main.dart
+++ dart files for testing/main.dart
@@ -1,38 +1,24 @@
-class person{
-  int id;
-  String name;
+// code from : https://medium.com/flutter-community/state-management-gymnastics-using-states-rebuilder-part-1-3ba3a6abf9c7
 
-  person.origin(){
-    id = 000;
-    name = "Default Name";
+import 'dart:math';
+
+class FutureCounterWithError {
+  int _counter = 0;
+
+  int get counter{
+    return _counter;
   }
 
-  int setval(){
-    id = 999;
-  }
-}
-
-class details extends person{
-  String address;
-
-  int setval(){
-    id = 990;
-  }
-
-  details.origin() : super.origin(){
-    id = 000;
-    name = 'Default Name';
+  void increment() async{
+    await Future.delayed(Duration(seconds: 1));
+    if(Random().nextBool()){
+      throw CounterError("There is an error in your counter");
+    }
   }
 
 }
 
-void main(){
-  var p = person.origin();
-  var d = details.origin();
-  print('Name is ${d.name}');
-  print('Address is ${d.address}');
-  p.setval();
-  d.setval();
-  print(d.id);
-  print(p.id);
+class CounterError extends Error{
+  final message;
+  CounterError(this.message);
 }
---
+++ enable_camera/lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:html';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Enable Camera with FAB",
+    home: EnableCamera(),
+  ));
+
+  var camera_list = availableCameras();
+  print(camera_list);
+
+}
+
+class EnableCamera extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Click for a pic"),
+      ),
+    );
+  }
+}
